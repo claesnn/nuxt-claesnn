52c536cfe9c51b1366494c202971eb25e7b899ab
Claes Nymand Nilsson <claes89@gmail.com>
2024-03-16 22:33:20 +0100

    Update nginx config to not have redirects

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,7 @@ onMounted(() =>
     </div>
     <footer class="footer border-t border-slate-200 mt-5">
         <div class="flex justify-between max-w-7xl mx-auto px-3 py-2 place-items-center">
-            <a href="https://github.com/claesnn/website" class="flex text-sm place-items-center">
+            <a href="https://github.com/claesnn/nuxt-claesnn" class="flex text-sm place-items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
                     <polyline points="16 18 22 12 16 6" />
